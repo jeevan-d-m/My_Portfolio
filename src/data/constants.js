96cabfd35958102607b776a4b4a58a83b8f79818
Jeevan D M <jeevanappu479@gmail.com>
2024-06-27 19:08:29 +0530

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,412 @@
+export const Bio = {
+  name: "Jeevan D M",
+  roles: [
+    "Tech Enthusiast",
+    "Software Student",
+    "Programmer",
+  ],
+  description:
+    "A passionate tech enthusiast with good programming and debugging skills, looking for an opportunity to\n" +
+      "leverage my skills and knowledge about current and emerging technologies in the software sector.\n",
+  github: "https://github.com/jeevan-d-m",
+  resume:
+    "https://drive.google.com/file/d/1csCXjwfexa9WSpU92VynvB9Sa8oCfuqD/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/jeevan-d-m-b619a5228?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+  twitter: "https://x.com/jeevan_dm_17?t=8pBEUiVUDjwTgkBlVz3X2A&s=09",
+  insta: "https://www.instagram.com/jeevan_appu17/",
+  facebook: "https://www.facebook.com",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript (Basics)",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+    ],
+  },
+
+
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Flask",
+        image:
+          "https://th.bing.com/th/id/OIP.mtZjjbjUD0RlF1CNbJA4uQHaF9?rs=1&pid=ImgDetMain",
+      },
+      {
+        name: "jinja2",
+        image:
+          "https://samuelcolvin.gallerycdn.vsassets.io/extensions/samuelcolvin/jinjahtml/0.20.0/1674478143852/Microsoft.VisualStudio.Services.Icons.Default",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+    ],
+  },
+
+
+  {
+    title: "Programming Languages",
+    skills: [
+
+      {
+        name: "C",
+        image:
+          "https://www.kindpng.com/picc/m/355-3559027_c-programming-language-logo-clipart-png-download-c.png",
+      },
+      {
+        name: "C++",
+        image:
+          "https://brandlogos.net/wp-content/uploads/2022/01/c-brandlogo.net_.png",
+      },
+      {
+        name: "java",
+        image: "https://th.bing.com/th/id/OIP.3pbn96F_IFdhSmJ1MaRe9AAAAA?rs=1&pid=ImgDetMain",
+      },
+        {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+    ],
+  },
+
+
+  {
+    title: "Machine Learning",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+
+      {
+        name: "Jupyter",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Jupyter_logo.svg/1767px-Jupyter_logo.svg.png",
+      },
+      {
+        name: "Google Colab",
+        image:
+          "https://main-educational.github.io/_images/logo_colab.png",
+      },
+      {
+        name: "Sk Learn Kit",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Scikit_learn_logo_small.svg/2560px-Scikit_learn_logo_small.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+
+      {
+        name: "Figma",
+        image:
+          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+      },
+      {
+        name: "Streamlit",
+        image:
+          "https://image.pngaaa.com/798/5084798-middle.png",
+      },
+
+        {
+        name: "Jetbrains IDE's",
+        image:
+          "https://logonoid.com/images/jetbrains-logo.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://lh3.googleusercontent.com/-aA5FKlzS_s8/Yy1PW8MUPzI/AAAAAAAAALs/lMBr7zu1LB05jS-oqvNdFsrZMk58Qwf1ACNcBGAsYHQ/s1600/1663913815516235-2.png",
+    role: "Project-Intern",
+    company: "Infosys",
+    date: "March 2024 - July 2024",
+    desc: "Developed a robust Fraud Detection System using machine learning algorithms to\n" +
+        "identify suspicious activities. Conducted data analysis and feature engineering to\n" +
+        "enhance model accuracy and reduce false positives.\n",
+    skills: [
+      "ML algorithms",
+      "Kaggle",
+      "Tableau",
+      "HTML",
+      "CSS",
+      "Flask",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Screenshot%20from%202024-01-25%2022-38-31.png?alt=media&token=2785903f-1a4e-41f5-afd2-6adcfe56d058",
+  },
+  {
+    id: 1,
+    img: "https://media-exp1.licdn.com/dms/image/C560BAQEVTlEYgQC5gQ/company-logo_200_200/0?e=2159024400&v=beta&t=O2p9wbLIN_sc1SYJAoBsroKSfjP874HDdeXAzcQfgUc",
+    role: "ML Project-Intern",
+    company: "IEEE Computer Society",
+    date: "April 2024 - Present",
+    desc: "Engineered VocalQuery, an AI-driven solution for extracting meaningful insights\n" +
+        "from audio files. Implemented natural language processing (NLP) and speech\n" +
+        "recognition technologies to analyze and transcribe audio data.\n",
+    skills: [
+      "LLM's",
+      "API",
+      "Cartesia AI",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/1696514649120.jpeg?alt=media&token=e7f6757b-edfa-4138-a692-d6709eeef3e2",
+  },
+
+  {
+    id: 5,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/girlScript.jpeg?alt=media&token=e656a621-cf3c-4230-bf0f-e74b4cec6035",
+    role: "Open Source Contributor ",
+    company: "GirlScript Summer of Code",
+    date: "May 2024 - Present",
+    desc: "Contributed to different open-source projects and learn from industry experts",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://th.bing.com/th/id/OIP.S84WAW6-lz3xcYum1L7n-AAAAA?rs=1&pid=ImgDetMain",
+    school: "Vidyavardhaka College of Engineering, Mysore",
+    date: "Oct 2021 - Sep 2025",
+    grade: "9.42 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Vidyavardhaka College of Engineering, Mysore. I have completed 5 semesters and have a CGPA of 9.42. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the IEEE Computer Society Student Chapter at VVCE, where I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://th.bing.com/th/id/OIP.TntR2QSQlux8qHAISbF1AAAAAA?rs=1&pid=ImgDetMain",
+    school: "Sadvidya Composite PU College, Mysore",
+    date: "Apr 2019 - Apr 2021",
+    grade: "94.33%",
+    desc: "I completed my class 12 high school education at Sadvidya Composite PU College, Mysore, where I studied Science with Biology",
+    degree: "12th, Science with Biology",
+  },
+  {
+    id: 2,
+    img: "https://th.bing.com/th/id/OIP.Bymy_PvM7zxi36mU9266iwHaHa?rs=1&pid=ImgDetMain",
+    school: "Jnana Sanjeevini High School, Nanjangud",
+    date: "Apr 2017 - Apr 2019",
+    grade: "91.52%",
+    desc: "I completed my class 10 education at Jnana Sanjeevini High School, Nanjangud, where I studied Science with Computer Application.",
+    degree: "10th, Science with Computer",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Anonymous Recruitment",
+    date: "Dec 2023",
+    description:
+      "A cutting-edge website crafted with Python Flask, Jinja2, Bootstrap, HTML, CSS and JS, designed to\n" +
+        "streamline the initial stages of recruitment while ensuring anonymity for job seekers.\n" +
+        "This ensures privacy of the candidates until Selection, thereby preventing data misuse.",
+    image:
+      "https://www.shrm.org/content/dam/en/shrm/topics-tools/news/talent-acquisition/202101-Talent-Trends-1_bi4qgd.gif",
+    tags: [
+      "HTML",
+        "CSS",
+        "JavaScript",
+        "Flask",
+        "Jinja2",
+        "SMTP",
+        "Werkzeug",
+        "SQLAlchemy"
+    ],
+    category: "web app",
+    github: "https://github.com/jeevan-d-m/Anonymous-Recruitment",
+    webapp: "https://github.com/jeevan-d-m/Anonymous-Recruitment",
+    member: [
+        {
+        name: "Jeevan D M",
+        img: "https://avatars.githubusercontent.com/u/96460761?v=4",
+        linkedin: "https://www.linkedin.com/in/jeevan-d-m-b619a5228/",
+        github: "https://github.com/jeevan-d-m",
+      },
+      {
+        name: "Yashas J Kumar",
+        img: "https://avatars.githubusercontent.com/u/96472550?s=400&u=84332ef1d5f8824b4d4ee09922ff68b5b87c9fca&v=4",
+        linkedin: "https://www.linkedin.com/in/yashasjkumar/",
+        github: "https://github.com/yashasjkumar/",
+      },
+      {
+        name: "Yashwanth Kumar A M",
+        img: "https://avatars.githubusercontent.com/u/68020051?v=4",
+        linkedin: "https://www.linkedin.com/in/yashwanthkumaram/",
+        github: "https://github.com/yashwanthkumaram/",
+      },
+      {
+        name: "Hariprasad Guttedar",
+        img: "https://avatars.githubusercontent.com/u/96561749?v=4",
+        linkedin: "https://www.linkedin.com/in/hariprasad-guttedar-aa112b24a/",
+        github: "https://github.com/Hariguttedar",
+      },
+
+    ],
+  },
+  {
+    id: 1,
+    title: "Medical Chatbot",
+    date: "Jan 2024",
+    description:
+      "A multi-lingual chatbot built with Llama2 , Gemini Pro Vision API ,\n" +
+        "Streamlit UI finetuned to respond to medical related queries",
+    image:
+      "https://static.wixstatic.com/media/a89add_3d73f7e43cff4f37bdf0af4772ef6595~mv2.gif",
+    tags: [
+        "Gen AI",
+        "LLM",
+        "Langchain",
+        "Streamlit",
+        "Google Translate",
+        "Pillow"
+    ],
+    category: "ai/ml",
+    github: "https://github.com/jeevan-d-m/AI-Medical-Chatbot",
+    webapp: "https://yunigma-med.streamlit.app",
+    member: [
+        {
+        name: "Jeevan D M",
+        img: "https://avatars.githubusercontent.com/u/96460761?v=4",
+        linkedin: "https://www.linkedin.com/in/jeevan-d-m-b619a5228/",
+        github: "https://github.com/jeevan-d-m",
+      },
+      {
+        name: "Yashas J Kumar",
+        img: "https://avatars.githubusercontent.com/u/96472550?s=400&u=84332ef1d5f8824b4d4ee09922ff68b5b87c9fca&v=4",
+        linkedin: "https://www.linkedin.com/in/yashasjkumar/",
+        github: "https://github.com/yashasjkumar/",
+      },
+      {
+        name: "Yashwanth Kumar A M",
+        img: "https://avatars.githubusercontent.com/u/68020051?v=4",
+        linkedin: "https://www.linkedin.com/in/yashwanthkumaram/",
+        github: "https://github.com/yashwanthkumaram/",
+      },
+      {
+        name: "Hariprasad Guttedar",
+        img: "https://avatars.githubusercontent.com/u/96561749?v=4",
+        linkedin: "https://www.linkedin.com/in/hariprasad-guttedar-aa112b24a/",
+        github: "https://github.com/Hariguttedar",
+      },
+
+    ],
+  },
+
+  {
+    id: 3,
+    title: "AI - Logistics Route Planner",
+    date: "May 2024",
+    description:
+      "Our project integrates advanced technologies to revolutionize supply chain operations. With Flask, we've\n" +
+        "created an intuitive interface for effortless interaction. Additionally, we harness the power of Large\n" +
+        "Language Models (LLMs) for output generation, providing insightful analyses and forecasts.",
+    image:
+      "https://i.pinimg.com/originals/c4/9a/20/c49a207e0f89c9290d98fd43a87a8cb0.gif",
+    tags: ["Flask", "Jinja2", "Werkzeug", "SMTP", "LLM", "Langchain", "Streamlit"],
+    category: "web app",
+    github: "https://github.com/jeevan-d-m/Logistics-Management",
+    webapp: "https://github.com/jeevan-d-m/Logistics-Management",
+    member: [
+        {
+        name: "Jeevan D M",
+        img: "https://avatars.githubusercontent.com/u/96460761?v=4",
+        linkedin: "https://www.linkedin.com/in/jeevan-d-m-b619a5228/",
+        github: "https://github.com/jeevan-d-m",
+      },
+      {
+        name: "Yashas J Kumar",
+        img: "https://avatars.githubusercontent.com/u/96472550?s=400&u=84332ef1d5f8824b4d4ee09922ff68b5b87c9fca&v=4",
+        linkedin: "https://www.linkedin.com/in/yashasjkumar/",
+        github: "https://github.com/yashasjkumar/",
+      },
+      {
+        name: "Yashwanth Kumar A M",
+        img: "https://avatars.githubusercontent.com/u/68020051?v=4",
+        linkedin: "https://www.linkedin.com/in/yashwanthkumaram/",
+        github: "https://github.com/yashwanthkumaram/",
+      },
+      {
+        name: "Hariprasad Guttedar",
+        img: "https://avatars.githubusercontent.com/u/96561749?v=4",
+        linkedin: "https://www.linkedin.com/in/hariprasad-guttedar-aa112b24a/",
+        github: "https://github.com/Hariguttedar",
+      },
+
+    ],
+  },
+
+    {
+    id: 3,
+    title: "Fraud Detection System",
+    date: "May 2024",
+    description:
+      "Developed a robust Fraud Detection System using machine learning algorithms to identify suspicious activities. Conducted data analysis and feature engineering to enhance model accuracy and reduce false positives.",
+    image:
+      "https://www.softwebsolutions.com/wp-content/uploads/2018/08/Fraud-detection.jpg",
+    tags: ["Flask","Machine Learning","Algorithms","Streamlit"],
+    category: "ai/ml",
+    github: "https://github.com/jeevan-d-m/Fraud_Detection_System",
+    webapp: "https://github.com/jeevan-d-m/Fraud_Detection_System",
+    member: [
+        {
+        name: "Jeevan D M",
+        img: "https://avatars.githubusercontent.com/u/96460761?v=4",
+        linkedin: "https://www.linkedin.com/in/jeevan-d-m-b619a5228/",
+        github: "https://github.com/jeevan-d-m",
+      },
+    ],
+  },
+];
